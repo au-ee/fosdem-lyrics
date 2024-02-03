21b104dd75e6851ae4eb114a56e8d4a72bcd8764
Markus Renschler <mr@toomanyissues.com>
2024-02-03 16:33:10 +0000

    make output colorful

--- demo-publisher/demopub.sh
+++ demo-publisher/demopub.sh
@@ -5,12 +5,21 @@
 
 # The NATS client uses the currently chosen context
 
+GREEN='\e[0;32m'
+GRAY='\e[0;30m'
+NC='\e[0m'
+
 while :
 do
     FILENAME="$(find lyrics -type f | shuf -n 1)"
     
+    printf  "${GREEN}New Song:"
+    head -n1 $FILENAME 
+    printf $GRAY
+    
     # publish to NATS
     cat $FILENAME | nats publish lyrics
   
+    printf $NC
     sleep 3
 done
